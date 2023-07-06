b025b7f036407ee7a07b99cbae5f6202c7829821
DungX <126141756+dungnot@users.noreply.github.com>
2023-07-07 02:33:23 +0700

    Update: Get List Time

--- src/main/java/com/hp/backend/repository/TimeRepository.java
+++ src/main/java/com/hp/backend/repository/TimeRepository.java
@@ -1,12 +1,17 @@
 package com.hp.backend.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.sql.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.hp.backend.entity.Times;
 
 @Repository
 public interface TimeRepository extends JpaRepository<Times, Integer> {
-
+    @Query("SELECT t FROM Times t WHERE t.session.session_id = :sessionId AND t.start_date = :startDate")
+    List<Times> findStartTimeAndEndTime(@Param("sessionId") int sessionId, @Param("startDate") Date startDate);
 }
