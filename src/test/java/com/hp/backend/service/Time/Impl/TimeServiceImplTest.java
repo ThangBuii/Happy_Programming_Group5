a9a00b0fdf0b8dd25af80a3594a16b19703b322c
Thang Bui <thangbdhe170642@fpt.edu.vn>
2023-07-20 02:48:45 +0700

    Merge

--- src/test/java/com/hp/backend/service/Time/Impl/TimeServiceImplTest.java
+++ src/test/java/com/hp/backend/service/Time/Impl/TimeServiceImplTest.java
@@ -22,12 +22,11 @@ import com.hp.backend.exception.custom.CustomBadRequestException;
 import com.hp.backend.model.time.dto.AddTimeRequestDTO;
 import com.hp.backend.model.time.dto.GetListTimeRequestDTO;
 import com.hp.backend.model.time.dto.GetListTimeResponseDTO;
-import com.hp.backend.model.time.dto.GetListTimeResponseFindMentorDTO;
 import com.hp.backend.model.time.mapper.TimeMapper;
 import com.hp.backend.repository.SessionRepository;
 import com.hp.backend.repository.TimeRepository;
 
-class TimeServiceImplTest {
+
 class TimeServiceImplTest {
     @Mock
     private TimeRepository timeRepository;
